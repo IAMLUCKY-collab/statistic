0244070f65f5854ae1cbdbf9794771bfef1176a0
IAMLUCKY-collab <shedracklucky100@gmail.com>
2023-11-09 15:00:57 +0100

    relate

---
+++ main.py
@@ -0,0 +1,23 @@
+### STATISTIC
+print("LUCKY AND CO. LIMITED")
+print("statistic for the number of drinks bought last month")
+print("Brand of drinks ")
+print("cola")
+print("pepsi")
+print("malt")
+print("bottle water")
+total_brand_of_drinks = float(input("total brand of drinks:  "))
+Angles_of_corresponding_sector = float(input("number of drinks /total brand of drinks*360"))
+no_of_creates = float(input("cola+pepsi+malt+bottle water"))
+
+sectors = ('cola / total brand of drinks * 360')
+sectors = ('pepsi / total brand of drinks * 360')
+sectors = ('malt / total brand of drinks * 360')
+sectors = ('bottle water / total brand of drinks * 360')
+
+print("LUCKY AND CO LIMITED")
+print(f'Simple interest : {sectors}')
+
+
+
+
